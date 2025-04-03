1993d6ed05b35f55a2a4af3ce42e25c0d9af7178
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2025-04-03 10:05:31 -0300

    Remove coding

--- src/components/Spacer/styles.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const stylesWithProps = (horizontal: boolean, size: number) => StyleSheet.create({
-    container: {
-        width: horizontal ? size : 0,
-        height: horizontal ? 0 : size,
-    },
-});
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './Button';
-export { Spacer } from './Spacer';
-export { Input } from './Input';
